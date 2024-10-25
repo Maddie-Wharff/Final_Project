5fccdead6bd770bf984f61e1cdefefede857efaa
Maddie-Wharff <maddie.wharff@ucas-edu.net>
2024-10-25 20:41:59 +0000

    oh no...
    Co-authored-by: EloraAnderson <EloraAnderson@users.noreply.github.com>
    Co-authored-by: CephasPetersen <CephasPetersen@users.noreply.github.com>

--- Final_project.py
+++ Final_project.py
@@ -1,27 +1,11 @@
-# By Elora, Maddie, Cephas, and Michael
-
-#extremely important things that I am adding!
-def gameEnd():
-        if wordGuess = word:
-                question = input(" Congratulations!\n you won!\n Would you like to restart?\n if so press y").strip().lower()
-                if question = "y":
-                        gameStart()
-                else:
-                        gameEnd
-        elif score = 6:
-                question = input(" Woops the word was {word}\n you lost.\n Would you like to restart?\n if so press y").strip().lower()
-                if question = "y":
-                        gameStart()
-                else:
-                        gameEnd
-        game()
+# Hangman, by Elora, Maddie, Cephas, and Michael
         
-# Pick a word - Both
+# Pick a word/ start the game - Michael
 import random
 wordlist = ["camel", "pizza", "turkey", "moon", "dream", "cat", "sweet", "tooth", "company","baboon", "gym", "belief", "leaf", "beef", "the", "dog", "in" , "fox"]
 word = ""
 wordGuess = ""
-score = 0
+points = 0
 def gameStart():
         global wordlist
         global word
@@ -36,64 +20,54 @@ def gameStart():
 # Let them pick a letter - All
 guess = input("Give me a letter: \n")
 
+# If they were wrong print a part of the man - Elora, Cephas
+def hangman():
+        global points
+        if points == 0:
+                print("\n----\n   |n|\n|\n|\n|\n|\n|\n--------")
+        elif points ==1:
+                print("\n-----\n|   |\n|   0\n|\n|\n|\n|\n|\n|\n-------")
+        elif points ==2:
+                print("\n-----\n|   |\n|   0\n|   -+-\n|\n|\n|\n|\n|\n-------")
+        elif points ==3:
+                print("\n-----\n|   |\n|   0\n| /-+-\n|\n|\n|\n|\n|\n-------")
+        elif points ==4:
+                print("\n-----\n|   |\n|   0\n| /-+-\ \n|\n|\n|\n|\n|\n-------")
+        elif points ==5:
+                print("\n-----\n|   |\n|   0 \n| /-+-\ \n|   |\n|\n|\n|\n|\n-------")
+        elif points ==6:
+                print("\n-----\n|   |\n|   0\n| /-+-\ \n|   |\n|   |\n|\n|\n|\n-------")
+        elif points ==7:
+                print("\n-----\n|   |\n|   0\n| /-+-\ \n|   |\n|   |\n|  |\n|\n|\n-------")
+        elif points ==8:
+                print("\n-----\n|   |\n|   0\n| /-+-\ \n|   |\n|   |\n|  |\n|  |\n|\n-------")
+        elif points ==9:
+                print("\n-----\n|   |\n|   0\n| /-+-\ \n|   |\n|   |\n|  |  |\n|  |\n|\n-------")
+        elif points ==10:
+                print("\n-----\n|   |\n|   0\n| /-+-\ \n|   |\n|   |\n|  |  |\n|  |  |\n|\n-------")
+
+
 # Check if letter is in word - Maddie    
 if guess in word:
         print("Correct!")
 else:
         print("Incorrect :(")
-
-# If they were wrong print a part of the man - Elora
-
-def hangman(): 
+        points = points + 1
+        hangman()
         
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
 
 # End the game - Michael
+def gameEnd():
+        if wordGuess == word:
+                question = input(" Congratulations!\n you won!\n Would you like to restart?\n if so press y").strip().lower()
+                if question =="y":
+                        gameStart()
+                else:
+                        gameEnd
+        elif points ==10:
+                question == input(" Woops the word was {word}\n you lost.\n Would you like to restart?\n if so press y").strip().lower()
+                if question =="y":
+                        gameStart()
+                else:
+                        gameEnd
+        game()
